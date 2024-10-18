addc158d94731f8002c5a37a4c56f0c685ed02e8
Anushareact <109461642+Anushareact@users.noreply.github.com>
2024-10-18 11:06:38 +0530

    Started Investement calculator project

--- react_coding_excercises/src/App.js
+++ react_coding_excercises/src/App.js
@@ -2,20 +2,24 @@ import "./App.css";
 import Examples from "./React_Practice/components/Examples";
 
 
-import Header from "./React_Practice/components/Header/Header";
+import Header from "../src/Projects/InvestmentCalculator/components/Header";
 import Input from "./React_Practice/components/Input";
 
 function App() {
  
   return (
     <>
-     
-      <Header />
+      {/* <Header />
       <main>
-      <Examples/>
+        <Examples />
       </main>
-      <Input type="text" placeholder="Enter your name"/>
-      <Input type="submit" text="Submit"></Input>
+      <p>
+        <Input type="text" placeholder="Enter your name" />
+      </p>
+      <p>
+        <Input type="submit" text="Submit"></Input>
+      </p> */}
+      <Header/>
     </>
   );
 }
